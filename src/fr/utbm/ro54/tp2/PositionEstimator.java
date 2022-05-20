f94a915d4f18f00528fe0d8822bf54f202e3f546
Firefoxx <erw.renault@gmail.com>
2022-05-20 18:47:17 +0200

    fixing bugs

--- src/fr/utbm/ro54/tp2/PositionEstimator.java
+++ src/fr/utbm/ro54/tp2/PositionEstimator.java
@@ -37,16 +37,16 @@ public class PositionEstimator {
 		
 		//compute alphas
 		ArrayList<Double> alphas = new ArrayList<Double>();
-		double m1 = cells.get(0).getMetric();
-		double m2 = cells.get(1).getMetric();
-		double m3 = cells.get(2).getMetric();
-		double m4 = cells.get(3).getMetric();
-		
-		double alpha1 = 1/(1 + m1/m2 + m1/m3 + m1/m4);
+		double alpha1 = 1;
+		for (int i = 1; i<K; i++) {
+			alpha1 += cells.get(0).getMetric() / cells.get(i).getMetric();
+		}
+		alpha1 = 1/alpha1;
 		alphas.add(alpha1);
-		alphas.add(alpha1 * m1 / m2);
-		alphas.add(alpha1 * m1 / m3);
-		alphas.add(alpha1 * m1 / m4);
+		
+		for (int i = 1; i<K; i++) {
+			alphas.add(alpha1*cells.get(0).getMetric()/cells.get(i).getMetric());
+		}
 
 		
 		//compute position vector
